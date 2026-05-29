25e9e6c6bebc98d4e305f2c6132c582913e2e9e2
nibirshahriar <nibir1891@gmail.com>
2026-05-29 18:20:44 +0600

    for testing purpose

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,10 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-export function proxy(request) {
-  const session = request.cookies.get("better-auth.session_token");
+export async function proxy(request) {
+  const session = await authClient.api.getSession({
+    headers: await headers(),
+  });
 
   if (!session) {
     return NextResponse.redirect(new URL("/signin", request.url));

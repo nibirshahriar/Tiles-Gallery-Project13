1ebc6d39beddb7fd5838c14120ebad47205e8ebd
nibirshahriar <nibir1891@gmail.com>
2026-05-29 16:54:48 +0600

    added my profile and updated

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+"use client";
+
+import { UpdateProfile } from "@/components/UpdateProfile";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
+import React from "react";
 
 const ProfilePage = () => {
-    return (
-        <div>
-            This is the profile page.
-        </div>
-    );
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+  return (
+    <div>
+      <Card className="max-w-96 mx-auto flex flex-col items-center gap-4 py-10 mt-5">
+        <Avatar className="h-20 w-20 rounded-full mx-auto">
+          <Avatar.Image
+            alt={user?.name}
+            src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>
+            {user?.name.charAt(0).toUpperCase()}
+          </Avatar.Fallback>
+        </Avatar>
+        <h2 className="text-xl font-bold">{user?.name}</h2>
+        <p className="text-muted-foreground">{user?.email}</p>
+        <UpdateProfile />
+      </Card>
+      ;
+    </div>
+  );
 };
 
 export default ProfilePage;
